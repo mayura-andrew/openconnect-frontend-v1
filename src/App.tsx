95c0d6d813d328eb52947e0147ec0b39ef49447a
mayura-andrew <mayuraalahakoon@gmail.com>
2024-06-08 20:27:05 +0530

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-blue-50 space-y-4">
-      <p className="text-blue-700 text-center text-4xl font-bold">OpenConnect</p>
+      <p className="text-blue-700 text-center text-4xl font-bold">Knowlihub</p>
       <p className="text-blue-600 text-center text-2xl">We are going to start our development of the platform</p>
     </div>
   );
